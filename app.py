6c48384aa385b8a36addeb1e4004fd4991b1a494
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2024-10-15 02:26:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def predict():
     return render_template('result.html',status=status,label=predicted_label)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
